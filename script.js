6e10e1f33603564d7eadb6ee18c4641041542cc4
John Joshua Ramirez <johnjoshuaramirez.social@gmail.com>
2022-12-29 06:48:21 -0800

    refactor: change some if statements into switch statements

--- script.js
+++ script.js
@@ -1,21 +1,3 @@
-function getComputerChoice() {
-	const choice = ["Rock", "Paper", "Scissors"];
-	const i = Math.floor(Math.random() * 3);
-	return choice[i];
-}
-
-function capitalize(word) {
-	let newWord = "";
-	for (let i = 0; i < word.length; i++) {
-		if (i === 0) {
-			newWord += word[i].toUpperCase();
-		} else {
-			newWord += word[i].toLowerCase();
-		}
-	}
-	return newWord;
-}
-
 const playerChoice = document.querySelector(".player-choice span");
 const computerChoice = document.querySelector(".computer-choice span");
 const h1 = document.querySelector(".container h1");
@@ -31,32 +13,61 @@ function playRound(e) {
 	const player = e.target.dataset.id;
 	const computer = getComputerChoice();
 
-	console.log(`Player choose ${player}`);
-	console.log(`Computer choose ${computer}`);
+	switch (player) {
+		case "Rock":
+			playerChoice.innerText = "✊";
 
-	if (player === "Rock") {
-		playerChoice.innerText = "✊";
-		if (computer === "Paper") {
-			computerChoice.innerText = "✋";
-			h1.innerText = "You Lose!";
-			p.innerText = "Paper beats Rock";
-			computerScore++;
-			roundNumber++;
-			computerScoreCount.innerText = computerScore;
-		} else if (computer === "Scissors") {
-			computerChoice.innerText = "✌️";
-			h1.innerText = "You Win!";
-			p.innerText = "Rock beats Scissors";
-			playerScore++;
-			roundNumber++;
-			playerScoreCount.innerText = playerScore;
-		} else {
-			computerChoice.innerText = "✊";
-			h1.innerText = "It's a Tie!";
-			p.innerText = `${player} ties with ${computer}`;
-			roundNumber++;
-		}
+			switch (computer) {
+				case "Paper":
+					computerChoice.innerText = "✋";
+					h1.innerText = "You Lose!";
+					p.innerText = "Paper beats Rock";
+					computerScore++;
+					roundNumber++;
+					computerScoreCount.innerText = computerScore;
+					break;
+
+				case "Scissors":
+					computerChoice.innerText = "✌️";
+					h1.innerText = "You Win!";
+					p.innerText = "Rock beats Scissors";
+					playerScore++;
+					roundNumber++;
+					playerScoreCount.innerText = playerScore;
+					break;
+
+				case "Rock":
+					computerChoice.innerText = "✊";
+					h1.innerText = "It's a Tie!";
+					p.innerText = `${player} ties with ${computer}`;
+					roundNumber++;
+               break
+			}
 	}
+
+	// if (player === "Rock") {
+	// 	playerChoice.innerText = "✊";
+	// 	if (computer === "Paper") {
+	// 		computerChoice.innerText = "✋";
+	// 		h1.innerText = "You Lose!";
+	// 		p.innerText = "Paper beats Rock";
+	// 		computerScore++;
+	// 		roundNumber++;
+	// 		computerScoreCount.innerText = computerScore;
+	// 	} else if (computer === "Scissors") {
+	// 		computerChoice.innerText = "✌️";
+	// 		h1.innerText = "You Win!";
+	// 		p.innerText = "Rock beats Scissors";
+	// 		playerScore++;
+	// 		roundNumber++;
+	// 		playerScoreCount.innerText = playerScore;
+	// 	} else {
+	// 		computerChoice.innerText = "✊";
+	// 		h1.innerText = "It's a Tie!";
+	// 		p.innerText = `${player} ties with ${computer}`;
+	// 		roundNumber++;
+	// 	}
+	// }
 
 	if (player === "Paper") {
 		playerChoice.innerText = "✋";
@@ -106,9 +117,9 @@ function playRound(e) {
 		}
 	}
 
-   removeEventListener();
+	removeEventListener();
 	colorizer();
-   animation();
+	animation();
 }
 
 const overlay = document.querySelector(".overlay");
@@ -138,11 +149,11 @@ const four = document.querySelector(".four");
 const five = document.querySelector(".five");
 
 function colorizer() {
-   const round = [one, two, three, four, five];
+	const round = [one, two, three, four, five];
 
-   for (let i = 0; i < roundNumber; i++) {
-      round[i].style.backgroundColor = "white";
-   }
+	for (let i = 0; i < roundNumber; i++) {
+		round[i].style.backgroundColor = "white";
+	}
 }
 
 function reset() {
@@ -164,9 +175,9 @@ function reset() {
 	playerScore = 0;
 	computerScore = 0;
 
-   buttons.forEach(button => {
-      button.addEventListener("click", playRound);
-   });
+	buttons.forEach(button => {
+		button.addEventListener("click", playRound);
+	});
 }
 
 const buttons = document.querySelectorAll("button");
@@ -175,24 +186,40 @@ buttons.forEach(button => {
 });
 
 function removeEventListener() {
-   if (roundNumber === 5) {
-      buttons.forEach(button => {
-         button.removeEventListener("click", playRound);
-         setTimeout(() => showModal(), 1200);
-      });
-   }
+	if (roundNumber === 5) {
+		buttons.forEach(button => {
+			button.removeEventListener("click", playRound);
+			setTimeout(() => showModal(), 1200);
+		});
+	}
 }
 
 function animation() {
-   playerChoice.classList.remove("active");
-   computerChoice.classList.remove("active");
-   setTimeout(() => {
-      playerChoice.classList.add("active");
-      computerChoice.classList.add("active");
-   }, 100)
-  
+	playerChoice.classList.remove("active");
+	computerChoice.classList.remove("active");
+	setTimeout(() => {
+		playerChoice.classList.add("active");
+		computerChoice.classList.add("active");
+	}, 100);
 }
 
 const modalButton = document.querySelector(".modal-button");
 modalButton.addEventListener("click", reset);
 
+function getComputerChoice() {
+	const choice = ["Rock", "Paper", "Scissors"];
+	const i = Math.floor(Math.random() * 3);
+	return choice[i];
+}
+
+function capitalize(word) {
+	let newWord = "";
+	for (let i = 0; i < word.length; i++) {
+		if (i === 0) {
+			newWord += word[i].toUpperCase();
+		} else {
+			newWord += word[i].toLowerCase();
+		}
+	}
+	return newWord;
+}
