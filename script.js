512edb5d4e933f78b9e2a76106260bd52aadee9e
John Joshua Ramirez <johnjoshuaramirez.social@gmail.com>
2022-12-30 06:28:29 -0800

    refactor: re order codebase

--- script.js
+++ script.js
@@ -2,13 +2,22 @@ const playerChoice = document.querySelector(".player-choice span");
 const computerChoice = document.querySelector(".computer-choice span");
 const h1 = document.querySelector(".container h1");
 const p = document.querySelector(".container p:nth-child(2)");
-const playerScoreCount = document.querySelector(".player-score span");
-const computerScoreCount = document.querySelector(".computer-score span");
 const playerScoreBars = document.querySelectorAll(".playerScoreBar");
 const computerScoreBars = document.querySelectorAll(".computerScoreBar");
+const buttons = document.querySelectorAll("button");
+const overlay = document.querySelector(".overlay");
+const result = document.querySelector(".modal h1");
+const modalPlayerScore = document.querySelector(".modal-player-score span");
+const modalComputerScore = document.querySelector(".modal-computer-score span");
+const modalButton = document.querySelector(".modal-button");
 
 const playerBar = Array.from(playerScoreBars);
 const computerBar = Array.from(computerScoreBars);
+
+modalButton.addEventListener("click", reset);
+buttons.forEach(button => {
+	button.addEventListener("click", playRound);
+});
 
 let playerScore = 0;
 let computerScore = 0;
@@ -85,10 +94,25 @@ function playRound(e) {
 	showModal();
 }
 
-const overlay = document.querySelector(".overlay");
-const result = document.querySelector(".modal h1");
-const modalPlayerScore = document.querySelector(".modal-player-score span");
-const modalComputerScore = document.querySelector(".modal-computer-score span");
+function animation() {
+	playerChoice.classList.remove("active");
+	computerChoice.classList.remove("active");
+
+	setTimeout(() => {
+		playerChoice.classList.add("active");
+		computerChoice.classList.add("active");
+	}, 100);
+}
+
+function incrementScoreBar() {
+	for (let i = 0; i < computerScore; i++) {
+		computerBar[i].style.backgroundColor = "white";
+	}
+
+	for (let i = 0; i < playerScore; i++) {
+		playerBar[i].style.backgroundColor = "white";
+	}
+}
 
 function showModal() {
 	if (playerScore === 3 || computerScore === 3) {
@@ -96,6 +120,7 @@ function showModal() {
 
 		setTimeout(() => {
 			overlay.classList.add("active");
+
 			modalPlayerScore.innerText = playerScore;
 			modalComputerScore.innerText = computerScore;
 
@@ -110,23 +135,22 @@ function showModal() {
 	}
 }
 
-function incrementScoreBar() {
-	for (let i = 0; i < computerScore; i++) {
-		computerBar[i].style.backgroundColor = "white";
-	}
-
-	for (let i = 0; i < playerScore; i++) {
-		playerBar[i].style.backgroundColor = "white";
+function removeEventListener() {
+	if (playerScore === 3 || computerScore === 3) {
+		buttons.forEach(button => {
+			button.removeEventListener("click", playRound);
+			button.classList.remove("hover", "active", "pointer");
+		});
 	}
 }
 
 function reset() {
 	playerChoice.innerText = "";
 	computerChoice.innerText = "";
+
 	h1.innerText = "Pick Your Choice";
 	p.innerText = "First To Score 3 Wins!";
-	// playerScoreCount.innerText = 0;
-	// computerScoreCount.innerText = 0;
+
 	overlay.classList.remove("active");
 
 	playerBar.forEach(bar => {
@@ -146,49 +170,9 @@ function reset() {
 	});
 }
 
-const buttons = document.querySelectorAll("button");
-buttons.forEach(button => {
-	button.addEventListener("click", playRound);
-});
-
-function removeEventListener() {
-	if (playerScore === 3 || computerScore === 3) {
-		buttons.forEach(button => {
-			button.removeEventListener("click", playRound);
-			button.classList.remove("hover", "active", "pointer");
-		});
-	}
-}
-
-// function slightPause() {
-// 	buttons.forEach(button => {
-// 		button.classList.remove("hover", "active", "pointer");
-// 		setTimeout(() => {
-// 			button.classList.add("hover", "active", "pointer");
-// 		}, 3000);
-// 	});
-// }
-
-function animation() {
-	playerChoice.classList.remove("active");
-	computerChoice.classList.remove("active");
-	setTimeout(() => {
-		playerChoice.classList.add("active");
-		computerChoice.classList.add("active");
-	}, 100);
-}
-
-const modalButton = document.querySelector(".modal-button");
-modalButton.addEventListener("click", reset);
-
-function getComputerChoice() {
-	const choice = ["Rock", "Paper", "Scissors"];
-	const i = Math.floor(Math.random() * 3);
-	return choice[i];
-}
-
 function capitalize(word) {
 	let newWord = "";
+
 	for (let i = 0; i < word.length; i++) {
 		if (i === 0) {
 			newWord += word[i].toUpperCase();
@@ -196,7 +180,15 @@ function capitalize(word) {
 			newWord += word[i].toLowerCase();
 		}
 	}
+
 	return newWord;
+}
+
+function getComputerChoice() {
+	const choice = ["Rock", "Paper", "Scissors"];
+	const i = Math.floor(Math.random() * 3);
+   
+	return choice[i];
 }
 
 function displayResult(choice, outcome, explain) {
@@ -206,11 +198,9 @@ function displayResult(choice, outcome, explain) {
 
 	if (outcome === "You Win!") {
 		playerScore++;
-		// playerScoreCount.innerText = playerScore;
 	}
 
 	if (outcome === "You Lose!") {
 		computerScore++;
-		// computerScoreCount.innerText = computerScore;
 	}
 }
